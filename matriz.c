e87b4f55142037e12bae8f0991a9a729a280b9eb
Pablo Padilla <pgpb.padilla@gmail.com>
2014-03-24 17:55:51 -0700

    Corrigiendo errores para poder compilar el programa.

--- matriz.c
+++ matriz.c
@@ -1,29 +1,63 @@
+#include<stdio.h>
+#include<stdlib.h>
+
 typedef int **matriz; 
 typedef int *pentero; 
  
 matriz generar_matriz(int m, int n) { 
   matriz aux; int i; 
-  if ((aux =(matriz)malloc(m*sizeof(pentero))) == NULL) 
-    return; 
-  for (i=0; i<m; i++) 
-    if ((aux[i] = (pentero)malloc(n*sizeof(int))) == NULL) 
-      return; 
+
+  /* Reserva memoria para `m` veces el tamanio de `pentero`.
+     `pentero` es de tipo `int*`, es decir, se esta reservando
+     memoria para `m` apuntadores a entero.
+     Si la reservacion es exitosa, `aux` es un apuntador de tipo `int**` que apunta al inicio del bloque reservado.
+     Si la reservacion de memoria falla, `aux` tendra el valor `NULL`
+   */
+  aux= (matriz)malloc( m*sizeof(pentero) );
+
+  /* Revisamos si la asignacion fue exitosa */
+  if ( aux == NULL) {
+    /* Si fallo la reservacion de memoria, la funcion regresa el valor `NULL` */
+    return NULL; 
+    /* Aqui se acaba el asunto */
+  }
+
+  /* Si la reservacion fue exitosa entonces `aux` es apuntador al inicio de un bloque de `m` apuntadores a enteros
+     Estas van a ser las columnas de la matriz */
+
+  /* Usamos un ciclo para reservar memoria para cada uno de los elementos de las filas */
+  for (i=0; i<m; i++) {
+      /* Reserva memoria para un bloque de `n` enteros
+	 El apuntador en la posicion `aux[i]` apunta al inicio del bloque recien reservado.
+       */
+    aux[i] = (pentero)malloc( n*sizeof(int) );
+  }
+    
+  /* Revisa si la reservacion fue exitosa */
+  if ( aux == NULL) {
+    return NULL; 
+  }
+  
+  /* Regresa el apuntador que apunta al inicio del bloque de enteros que representa */
+  /* una matriz de `m` columnas y `n` filas */
   return aux; 
 } 
  
 void liberar_matriz(matriz p, int m, int n){ 
   int i; 
-  for(i=0; i<m; i++) 
+  for(i=0; i<m; i++) {
     free(p[i]); 
-  free(p) 
-    } 
- 
+  }
+
+  free(p);
+} 
+
 void leer_matriz(matriz p, int m, int n){ 
   int i, j; 
   for (i=0; i<m; i++){ 
-    for (j=0; i<n; j++){ 
-      printf(“p[%d][%d]:”,i,j); 
-      scanf(“%d”,&(p[i][j])); 
+    for (j=0; j<n; j++){ 
+      printf("p[%d][%d]:",i,j); 
+      scanf("%d",&(p[i][j])); 
     } 
   } 
 } 
@@ -32,42 +66,57 @@ void leer_matriz(matriz p, int m, int n){
 void mostrar_matriz(matriz p, int m, int n){ 
   int i, j; 
   for (i=0; i<m; i++){ 
-    for (j=0; i<n; j++){ 
-      printf(“%d ”,p[i],p[j]); 
+    for (j=0; j<n; j++){ 
+      printf("%d ",p[i][j]); 
  
     } 
-    printf(“\n”); 
+    printf("\n"); 
   } 
 } 
  
-void sumar_matrices(matriz A, matriz b, int m, int n) 
+void sumar_matrices(matriz A, matriz B, int m, int n) {
   int i, j; 
-for (i=0; i<m; i++){ 
-  for (j=0; i<n; j++){ 
-    A[i][j] = A[i][j] + B[i][j]; 
+  for (i=0; i<m; i++){ 
+    for (j=0; j<n; j++){ 
+      A[i][j] = A[i][j] + B[i][j]; 
+    } 
   } 
- } 
 } 
  
 int main (){ 
-  int m, n; matriz A, matriz B; 
-  printf(“FILAS:”); 
-  scanf(“%d”, &m); 
-  printf(“COLUMNAS:”); 
-  scanf(“%d”, &n); 
-  A = generar_matriz(m,n); 
-  B=generar_matriz(m,n); 
-  printf(“DATOS DE LA MATRIZ A:\n”); 
-  leer_matriz(A,m,n); 
-  printf(“DATOS DE LA MATRIZ B:\n”); 
-  leer_matriz(B,m,n); 
-  sumar_matrices(A,B,m,n); 
-  printf(“La suma de A y B es:\n”); 
-  mostrar_matriz(A,m,n); 
-  printf(“LIBERANDO LAS MATRICES:\n”); 
-  liberar_matriz(A,m,n); 
-  A=NULL; 
-  liberar_matriz(B,m,n); 
-  B=NULL; return 0; 
+  int m, n; 
+  matriz A, B; 
 
+  printf("FILAS:"); 
+  scanf("%d", &m); 
+
+  printf("COLUMNAS:"); 
+  scanf("%d", &n); 
+
+  A = generar_matriz(m,n); 
+  B= generar_matriz(m,n); 
+
+  printf("DATOS DE LA MATRIZ A:\n"); 
+  leer_matriz(A,m,n); 
+  mostrar_matriz(A,m,n); 
+
+  printf("DATOS DE LA MATRIZ B:\n"); 
+  leer_matriz(B,m,n); 
+  mostrar_matriz(B,m,n); 
+
+  sumar_matrices(A,B,m,n); 
+
+  printf("La suma de A y B es:\n"); 
+  mostrar_matriz(A,m,n); 
+
+  printf("LIBERANDO LAS MATRICES:\n"); 
+
+  liberar_matriz(A,m,n); 
+
+  A=NULL; 
+
+  liberar_matriz(B,m,n); 
+
+  B=NULL; 
+  return 0; 
 } 

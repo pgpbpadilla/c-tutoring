727ba6dd12c571ad6afae0ac590e572d9deb027a
Pablo Padilla <pgpb.padilla@gmail.com>
2015-07-09 23:39:54 -0700

    new: dev: implement getNext method. TODO: break down program into smaller programs and create own file for TreeNode class.

--- ctci/4.3-create-bst.cc
+++ ctci/4.3-create-bst.cc
@@ -40,7 +40,7 @@ public:
     stringstream result;    
     
     TreeNode* l = node;
-    cout<< "string for node:" << node->data << endl;
+    // cout<< "string for node:" << node->data << endl;
 
     // cout << "go left" << endl;
     result << this->stringFromTreeInOrder(l->left) << " ";
@@ -55,12 +55,12 @@ public:
 
   string toString(TraversalType traversalType = TraversalType::InOrder){
 
-    cout<< "toString: " << endl;
+    // cout<< "toString: " << endl;
 
     switch (traversalType) {
     default:
       {
-        cout << "traversal: InOrder" << endl;
+        // cout << "traversal: InOrder" << endl;
         return this->stringFromTreeInOrder(this);
       }
     }
@@ -85,7 +85,61 @@ TreeNode* buildTree(vector<int> numbers, int start, int end) {
   root->left = buildTree(numbers, start, midPoint - 1);
   root->right = buildTree(numbers, midPoint + 1, end);
 
+  if (nullptr != root->left) {
+    root->left->parent = root;
+  }
+
+  if (nullptr != root->right) {
+    root->right->parent = root;
+  }
+
   return root;
+}
+
+TreeNode* getNext(TreeNode* node) {
+  TreeNode * next = nullptr;
+
+  cout << "getNext: " << node->data << endl;
+
+  if (node->right != nullptr) {
+
+    cout << "try on the right subtree" << endl;
+    next = node->right;
+    
+    while (nullptr != next->left) {
+      cout << "go left from: " << next->data  << endl;
+      next = next->left;
+    }
+    
+    return next;
+    
+  } else { // there is no right child node
+
+    cout << "try looking up" << endl;
+    // start the search in the parent node
+    next = node->parent;
+
+    // if this is the root node
+    // then there's no greater node
+    if (nullptr == next) {
+      cout << "reached null" << next << endl;
+      return nullptr;
+    }
+    
+    // this is not the root node
+    // keep going up until you find the root or a node
+    // that's greater than the given node
+    while (nullptr != next
+           && next->data < node->data) {
+      cout << "go up: " << next->data << endl;
+      next = next->parent;
+    }
+    
+    // return the first parent node
+    // that is greater than the given node
+    cout << "next found: " << (nullptr == next ? -1 : next->data) << endl;
+    return next;
+  }
 }
 
 int main(int argc, char* argv[]) {
@@ -112,5 +166,19 @@ int main(int argc, char* argv[]) {
   cout << "tree root: " << (nullptr == root ? -1 : root->data) << endl;
   cout << root->toString() << endl;
   
+  TreeNode* next = getNext(root);
+
+  if (nullptr != next) {
+    cout << next->data << endl;
+  }
+  
+  // cout<< root->left->data << endl;
+  // cout<< root->right->data << endl;
+
+  next = getNext(root->left->left->right);
+  if (nullptr != next) {
+    cout << next->data << endl;
+  }  
+
   return 0;
 }

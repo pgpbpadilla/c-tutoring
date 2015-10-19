490891a816cbe07722348364c4eb3c0506f5e05d
Pablo Padilla <pgpb.padilla@gmail.com>
2015-10-18 20:58:20 -0700

    chg: usr: simplificar uso de cuentas

--- java/Bankito/Bankito.java
+++ java/Bankito/Bankito.java
@@ -3,8 +3,9 @@ public class Bankito{
     public static void main(String[ ] args){
         Cliente cliente1 = new Cliente("Mirna","xs0098");
 
-        Cuenta cta = new Cuenta(500.85);
-        cliente1.establecerCuenta(cta);
+        Cuenta cta1 = new Cuenta(500.85);
+
+        cliente1.establecerCuenta(cta1);
 
         System.out.println("Nombre del Cliente: "  + cliente1.obtenerNombre() + "\n\n"
                            + "\tNo. Cuenta: " + cliente1.obtenerNumCta() + "\n"

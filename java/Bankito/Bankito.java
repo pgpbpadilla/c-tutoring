02d22356ef09039c2c1f9365059ffc277c44ec01
Pablo Padilla <pgpb.padilla@gmail.com>
2015-10-18 20:35:14 -0700

    chg: dev: Simplificar uso de la cuenta.

--- java/Bankito/Bankito.java
+++ java/Bankito/Bankito.java
@@ -1,29 +1,15 @@
 public class Bankito{  
 
-
     public static void main(String[ ] args){
-        Cuenta ctaAux; 
-        Cuenta ctaAux2;
-			
-        Cliente qx= new Cliente("El Quijote","QX400");
-        Cliente sx=new Cliente("Sancho Panza","SPANZA");
-				
-        qx.establecerCuenta(new Cuenta(550.45));
-        sx.establecerCuenta(new Cuenta(100.35));
-							
-        ctaAux= qx.obtenerCuenta( );
-        ctaAux2= sx.obtenerCuenta( );
-	
-        System.out.println("Cliente:" + qx.obtenerNombre());
+        Cliente cliente1 = new Cliente("Mirna","xs0098");
 
-        /*ctaAux.depositar(300.55);
-          ctaAux2.retirar(100);
-          ctaAux.depositar(49);*/
-        System.out.println("Nombre Cliente:" +qx.obtenerNombre());
-        System.out.println("Num cta: " +qx.obtenerNumCta());
-        System.out.println("Saldo: "+ctaAux.consultar());
-        System.out.println("Nombre Cliente:" +sx.obtenerNombre());
-        System.out.println("Num cta: " +sx.obtenerNumCta());
-        System.out.printf("Saldo: %.2f\n", ctaAux2.consultar());
+        Cuenta cta = new Cuenta(500.85);
+        cliente1.establecerCuenta(cta);
+
+        System.out.println("Nombre del Cliente: "  + cliente1.obtenerNombre() + "\n\n"
+                           + "\tNo. Cuenta: " + cliente1.obtenerNumCta() + "\n"
+                           + "\tSaldo: " +  cliente1.obtenerCuenta().consultar() + "\n");
+	
     } 
+
 } 

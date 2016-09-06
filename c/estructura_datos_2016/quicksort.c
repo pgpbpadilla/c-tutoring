a6e98025eeb1bb121740838921ec2f0f67b7fdda
Pablo Padilla <pgpb.padilla@gmail.com>
2016-09-05 20:29:07 -0700

    chg: dev: Generalize algorithm to sort any number of integers using dynamic memory management

--- c/estructura_datos_2016/quicksort.c
+++ c/estructura_datos_2016/quicksort.c
@@ -1,4 +1,6 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
 
 void print_array(int numeros[], int cantidad, char* titulo){
   printf("\n%s:\n\t[ ", titulo);
@@ -45,11 +47,46 @@ void quicksort(int numeros[], int min, int max) {
   
 }
 
-int main (void) {
+void llenar_arreglo(int numeros[], int cantidad){
 
-  int numeros[] = {5, 7, 6, 1, 8, 4, 9, 3, 2, 0};
+  int i;
+  /* initialize the random number generator */
+  srand(time(NULL)); /* generate the seed */
+  /* [0, RAND_MAX] => [0, 100] */
+  double factor_escala = (double)RAND_MAX / (double)100;
+  int r, n;
   
-  print_array(numeros, 10, "Antes de ordenar");
-  quicksort(numeros, 0, 9);
-  print_array(numeros, 10, "Despues de ordenar");
+  for(i=0; i<cantidad; i++){
+    
+    r = rand(); /* r in [0, RAND_MAX] */
+    n = (int) ((double)r / factor_escala); /* n in [0, 100] */
+    printf("\nGenerado el numero: %d", n);
+    numeros[i] = n;
+  }
+
+}
+
+int main (void) {
+  
+
+  int *numeros;
+  int cantidad;
+
+  printf("Cuantos numeros desea ordenar?:");
+  scanf("%d", &cantidad);
+
+  numeros = (int *)malloc(cantidad * sizeof(int));
+  
+  if (NULL == numeros) {
+    printf("Error creando arreglo de numeros!!!!! FATAL ERRRRORRORORR");
+    exit(-1);
+  }
+
+  llenar_arreglo(numeros, cantidad);
+  print_array(numeros, cantidad, "Antes de ordenar");
+
+  quicksort(numeros, 0, cantidad-1);
+  print_array(numeros, cantidad, "Despues de ordenar");
+
+  free(numeros);
 }

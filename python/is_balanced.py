119fb28e6611ad99a289623112e09c8c37a44562
Pablo Padilla <pgpb.padilla@gmail.com>
2017-06-18 12:42:02 -0700

    new: dev: Anki CompSci: Which strings would the code think are balanced?

---
+++ python/is_balanced.py
@@ -0,0 +1,24 @@
+import sys
+
+input = sys.argv[1]
+
+print('Input: {}'.format(input))
+
+stack = []
+
+for c in input:
+    print('processing {}'.format(c))
+
+    if c == '(':
+        stack.append(c)
+        print('push {}'.format(c))
+    elif c == ')' and len(stack) > 0:
+        pc = stack.pop()
+        print('pop {}'.format(pc))
+    else:
+        sys.exit('Unbalanced')
+
+    print('stack: {}'.format(stack))
+    
+print('Balanced!, Yay!')
+
